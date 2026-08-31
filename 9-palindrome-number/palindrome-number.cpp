c21d5054166cb578fb6fdbe87088765f1ea0a94a
Naina modi <144493792+Nainamodi20@users.noreply.github.com>
2026-08-31 19:25:37 +0530

    Time: 2 ms (50.06%) | Memory: 8.4 MB (92.15%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original;
+        long long reverse = 0;
+        int remainder;
+        original = x;
+        while (x > 0) {
+            remainder = x % 10;
+            reverse = reverse * 10 + remainder;
+            x = x / 10;
+
+        }
+        if (reverse == original)
+            return true;
+        else
+            return false;
+    }
+};
