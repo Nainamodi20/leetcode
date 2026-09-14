5e256dc92905a30ffe1aad6f071f18161e6dddef
Naina modi <144493792+Nainamodi20@users.noreply.github.com>
2026-09-14 13:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (47.04%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.cpp
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-            // if(rec2[0] < rec1[2] && rec1[i] < rec2[2]) return true;
-            // if(rec1[0] < rec2[i] && rec2[i] < rec1[2]) return true;
             if(rec1[2] <= rec2[0] || rec1[0] >= rec2[2] || rec1[1] >= rec2[3] || rec1[3] <= rec2[1]) return false;
             else return true;
     }
 };
+
+//agar dono rectangle k axis overlap kr rha h. toh jaruri ni h ki dono rec intersect kre. hm check krenge ki rec1 ka x rec2 k x se chota h and same goes with y. agar ye condition follow hota h toh overlapping ni ho rhi. wrna ho rha h. 
