f121242a67228735563899d92cf2df65ae895c87
Naina modi <144493792+Nainamodi20@users.noreply.github.com>
2026-09-14 13:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (77.39%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+            // if(rec2[0] < rec1[2] && rec1[i] < rec2[2]) return true;
+            // if(rec1[0] < rec2[i] && rec2[i] < rec1[2]) return true;
+            if(rec1[2] <= rec2[0] || rec1[0] >= rec2[2] || rec1[1] >= rec2[3] || rec1[3] <= rec2[1]) return false;
+            else return true;
+    }
+};
