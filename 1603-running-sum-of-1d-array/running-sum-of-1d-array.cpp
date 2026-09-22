935f7218f00323ca74bc3aca0d538a1a63476909
Naina modi <144493792+Nainamodi20@users.noreply.github.com>
2026-09-22 22:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (81.15%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> sum;
+        int total = 0;
+        for(int i = 0; i < size(nums); i++){
+            total += nums[i];
+            sum.push_back(total);
+        }
+        return sum;
+    }
+};
