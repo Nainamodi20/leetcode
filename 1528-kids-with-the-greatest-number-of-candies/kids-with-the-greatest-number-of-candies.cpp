57713766796da46354c5ac8eeff9ef552e0d6294
Naina modi <144493792+Nainamodi20@users.noreply.github.com>
2026-09-26 13:39:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (26.40%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        vector<bool> result;
+        for (int i = 0; i < size(candies); i++){
+            if((candies[i] + extraCandies) >= maxCandies){
+            result.push_back(true);
+            } else
+            result.push_back(false);
+        }
+    return result;
+    }
+};
